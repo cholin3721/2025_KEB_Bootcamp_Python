dea6f350d112d55615657b41cf7322168bc6f487
cholin3721 <cholin3721@gmail.com>
2025-02-11 10:33:33 +0900

    v0.4 create day02.py(gugudan)

---
+++ day02.py
@@ -0,0 +1,10 @@
+# for dan in range(2 , 10, 1) :
+#     for i in range (1, 10, 1) :
+#         print(f"{dan} * {i} = {dan*i}")
+#     print()
+# 구구단
+
+dan = int(input("단을 입력하시오 : "))
+for i in range (1, 10, 1) :
+      print(f"{dan} * {i} = {dan*i}")
+print()
