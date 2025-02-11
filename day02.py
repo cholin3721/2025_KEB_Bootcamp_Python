85c25e0ca19f4c6da749cee59d7a6e9e5ffa90aa
cholin3721 <cholin3721@gmail.com>
2025-02-11 17:25:14 +0900

    v1.4 PrimeNumber Test

--- day02.py
+++ day02.py
@@ -1,5 +1,19 @@
 # prime number
 
+def my_pow(num1, num2) -> float:
+    result = 1
+    for _ in range(num2) :
+        result *=  num1
+    return result
+
+def is_prime(n1) -> bool :
+    i = 2
+    while my_pow(i, 2) <= n1:
+        if n1 % i == 0:
+            return False
+        i = i + 1
+    return True
+
 numbers = input("Input first second number ex -> 15 24) : ").split()
 n1 = int(numbers[0])
 n2 = int(numbers[1])
@@ -11,13 +25,6 @@ else :
     pass
 
 while n1 <= n2 :
-    i = 2
-    is_Prime = True
-    while pow(i, 2) <= n1 :
-        if n1 % i == 0 :
-            is_Prime = False
-            break
-        i = i + 1
-    if is_Prime :
+    if is_prime(n1) :
         print(n1, end=" ")
-    n1 = n1 + 1
+    n1 += 1
