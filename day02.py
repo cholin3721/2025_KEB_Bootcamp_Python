5e978de53a8712691a09623f0bbe2b7f2c62dacd
cholin3721 <cholin3721@gmail.com>
2025-02-11 11:41:31 +0900

    v0.7 PrimeNumber better

--- day02.py
+++ day02.py
@@ -10,10 +10,15 @@
 # print()
 # 입력받은 숫자의 구구단
 
+import math as m
+
 num = int(input("Input Number : "))
 is_prime = True
 if num >= 2 :
-      for i in range(2, num) :
+      # for i in range(2, int(n**0.5)+1): 이것도 가능함 **은 제곱, 0.5를 제곱하면 결국은 제곱근 구하게 됨.
+      for i in range(2, int(m.sqrt(num)+1)) :  #여기서 제곱근을 포함 안시키면 9 같은건 1, 3, 9 이기 때문에 3까지 검사 안하고 소수판정 내림.
+
+            # if num % (i**2) : for문을 유지했다 했을때, i를 제곱했을때도 가능하다
             if num % i == 0 :
                   is_prime = False
                   break
@@ -24,3 +29,6 @@ if is_prime :
       print(f"{num} is Prime Number")
 else :
       print(f"{num} is NOT Prime Number")
+
+print(2**10)
+print(16**2)
