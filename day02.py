4a5fc82bde4772994345b41ed64d7e02b2a53a53
cholin3721 <cholin3721@gmail.com>
2025-02-11 13:31:59 +0900

    v0.8 PrimeNumber, Help, is_Prime, documentation, Function

--- day02.py
+++ day02.py
@@ -10,25 +10,36 @@
 # print()
 # 입력받은 숫자의 구구단
 
-import math as m
+def is_prime(num) -> bool: # 보통 이렇게 명시적으로 써서 가독성을 높임
+      """
+      this function is for judge PrimeNumber, if return is true, it is.
+      :param num: integer number
+      :return: boolean
+      """
+      if num >= 2:
+            # for i in range(2, math.sqrt(num)+1): 이것도 가능함 **은 제곱, 0.5를 제곱하면 결국은 제곱근 구하게 됨.
+            for i in range(2, int(num**0.5 + 1)):  # 여기서 제곱근을 포함 안시키면 9 같은건 1, 3, 9 이기 때문에 3까지 검사 안하고 소수판정 내림.
+                   # if num % (i**2) : for문을 유지했다 했을때, i를 제곱했을때도 가능하다
+                  if num % i == 0:
+                        # is_prime = False
+                        # break
+                        return False
+      else:
+            return False
+
+      return True
+help(abs)  # help 함수의 대한 설명을 출력해주는 함수
+help(is_prime)  # 내가 만든 함수도 됨 ㅋㅋㅋㅋㅋㅋㅋㅋㅋ 이건 몰랐네 ''' ''' 여기 안에 있는 것들이 출력됨
 
 num = int(input("Input Number : "))
-is_prime = True
-if num >= 2 :
-      # for i in range(2, int(n**0.5)+1): 이것도 가능함 **은 제곱, 0.5를 제곱하면 결국은 제곱근 구하게 됨.
-      for i in range(2, int(m.sqrt(num)+1)) :  #여기서 제곱근을 포함 안시키면 9 같은건 1, 3, 9 이기 때문에 3까지 검사 안하고 소수판정 내림.
 
-            # if num % (i**2) : for문을 유지했다 했을때, i를 제곱했을때도 가능하다
-            if num % i == 0 :
-                  is_prime = False
-                  break
-else :
-      is_prime=False
-
-if is_prime :
+if is_prime(num):
       print(f"{num} is Prime Number")
 else :
       print(f"{num} is NOT Prime Number")
 
 print(2**10)
 print(16**2)
+
+# solid(srp, ocp, lsp, isp,dip) 객체 지향 프로그래밍 5대 원칙
+# srp : 하나의 함수의 하나의 기능만 넣어놔야 한다. side effect = 부작용 을 걱정하기 때문
