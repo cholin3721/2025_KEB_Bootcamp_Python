c6be4ae7f76a4dc20fd1add1a9cdd5d029042363
cholin3721 <cholin3721@gmail.com>
2025-02-12 09:08:32 +0900

    v1.5 PrimeNumber complete!

--- day02.py
+++ day02.py
@@ -1,12 +1,26 @@
 # prime number
 
 def my_pow(num1, num2) -> float:
+    '''
+    :param num1  : basenumber
+    :param num2  : exponent
+    :return float: the power result iin the form of a real number
+    alternate the pow()
+    '''
+
     result = 1
     for _ in range(num2) :
         result *=  num1
     return result
 
+
+# 함수와 함수 사이는 2줄 띄는게 국룰
 def is_prime(n1) -> bool :
+    '''
+    :param n1 int:
+    :return:
+
+    '''
     i = 2
     while my_pow(i, 2) <= n1:
         if n1 % i == 0:
@@ -20,9 +34,6 @@ n2 = int(numbers[1])
 
 if n1 > n2 :
     n1, n2 = n2, n1
-
-else :
-    pass
 
 while n1 <= n2 :
     if is_prime(n1) :
