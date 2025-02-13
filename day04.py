485e4e401c00d1f40f233bc96f87eaaffbf6a834
cholin3721 <cholin3721@gmail.com>
2025-02-13 11:15:04 +0900

    v0.2 drinks order final

---
+++ day04.py
@@ -0,0 +1,154 @@
+# d_s_p = {"위스키" : ['초콜릿', 50000]}
+# print(d_s_p["위스키"][1])
+
+drinks_foods = ["초콜릿","치즈","삼겹살","양꼬치"]
+drinks = ["위스키", "와인", "소주", "고량주"]
+prices = [50000, 30000, 5000, 7500]
+bill = {}
+
+
+import random
+import math
+
+def print_bill() :
+    amount = 0
+    print()
+    print("영수증")
+    print("*" * 20)
+    for a, b in bill.items() :
+        print(f"{a} + {drinks_foods[drinks.index(a)]}\n {b:<2} 세트 | {b * prices[(drinks.index(a))]:<10}")
+        amount +=b * prices[(drinks.index(a))]
+    print(f"\n지불 금액 : {amount} 원")
+    print("*" * 20)
+    print("감사합니다")
+    print()
+
+
+def make_bill(menu) :
+    if drinks[menu] in list(bill.keys()) :
+        bill[drinks[menu]] += 1
+    else :
+        bill[drinks[menu]] = 1
+
+
+def how_bout_this(drinks, drinks_food, menu):
+    print(f'{drinks[menu]} 에는 {drinks_foods[menu]} 이거지 ㅋㅋ')
+    print()
+    make_bill(menu)
+
+
+def delete(drink, food) :
+    print_menu(drink,food)
+
+    try :
+        num = math.floor(float(input("몇번 술이랑 안주를 삭제할까요?\n-->").strip()))
+        del drink[num-1]
+        del food[num-1]
+
+    except Exception :
+        print("올바르게 입력해주세요!")
+        return
+
+
+def print_menu(drink, food) :
+    print_list = [(a, b, f"가격 : {c}원") for a, b, c in zip(drink, food, prices)]
+
+    for j, doprint in enumerate(print_list):
+        print(f'{j + 1}번 메뉴 :', end =" ")
+        print(doprint)
+
+    print()
+
+
+def ask_modify(drink, food) :
+    print_menu(drink, food)
+    try :
+        pick = math.floor(float(input("몇 번 술의 안주를 수정할까요? : ").strip()))
+        if pick < 1 or pick > len(drinks) :
+            print("해당 번호가 없습니다")
+            return
+
+    except Exception :
+        print("해당 번호가 없습니다.")
+        return
+
+    modify(drink, food, pick-1)
+
+
+def modify(drink, food, pick) :
+    anjoo = input("뭘로 수정할까요? : ").strip()
+    food[pick] = anjoo
+    print(f"{drink[pick]}엔 역시 {food[pick]} 이지~")
+    print()
+
+    choice = input("가격도 수정하시겠어요? (Y/N)").strip().upper()
+    if choice == 'Y' :
+        price = int(input("얼마로 수정하시겠어요 ? : "))
+        prices[pick] = price
+    else :
+        print("수정을 종료합니다.")
+        return
+
+
+def add(drink, food) :
+    alcohol = input("어떤 술을 추가하실껀가요 ? : ").strip()
+    if alcohol in drink :
+        menu = input("이미 술이 존재합니다, 수정하시겠어요? Y/N : ").strip().upper()
+        if menu == 'Y' :
+            modify(drink, food, drink.index(alcohol))
+        else :
+            return
+    else :
+        drink.append(alcohol)
+        anjoo = input("어떤 안주를 추가하실껀가요 ? : ").strip()
+        price = int(input("얼마인가요 ? : ").strip())
+        food.append(anjoo)
+        prices.append(price)
+
+
+while True:
+    print(f'다음 선택지 중에 고르세요. ')
+
+
+    menu_list = ""
+    j = 0
+    for i, drink in enumerate(drinks) :
+        menu_list += f'{i+1}.{drink} '
+        j = i
+    menu_list += f'\n\n{j+2}.아무거나, {j+3}.이건 아니지! 수정 {j+4}.이게 없네~ 추가 {j+5}.넌 나가라~ 제거 {j+6}.전체 출력!'
+    menu_list += f"\n\n{j+7}.주문 내역 {j+8}.종료 "
+    try :
+        menu = math.floor(float(input(menu_list + "\n--> ")))
+
+    except Exception :
+        print("다시 입력해주세요!")
+        continue
+
+    if menu >= 1 and menu <= len(drinks):
+        how_bout_this(drinks, drinks_foods, menu-1)
+
+    elif menu == j+2 :
+        num = random.randint(0, len(drinks)-1)
+        how_bout_this(drinks, drinks_foods, num)
+
+    elif menu == j+3 :
+        ask_modify(drinks, drinks_foods)
+
+    elif menu == j+4 :
+        add(drinks, drinks_foods)
+
+    elif menu == j+5 :
+        delete(drinks, drinks_foods)
+
+    elif menu == j+6 :
+        print_menu(drinks, drinks_foods)
+
+    elif menu == j+7 :
+        print_bill()
+
+    elif menu == j+8 :
+        break
+
+    else :
+        print("올바른 번호를 입력해주세요!")
+
